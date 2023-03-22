3434796131da9cfafc2e138f2bbaa4670483e5c3
alinaincodeland <alinacuznetov@gmail.com>
2023-03-22 11:00:21 +0100

    further changes.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -139,7 +139,7 @@ export default function Navbar() {
                               .then((res) => {
                                 dispatch({ type: "LOGOUT" });
                                 // dispatch({ type: "CLEAR" });
-                              })
+                              }),
                           )
                           .catch((err) => {
                             console.log(err);
