245c0f3412fb333e3581f2c22686ae767095f11e
alinaincodeland <alinacuznetov@gmail.com>
2023-04-02 15:49:55 +0200

    Created the first post.

--- client/src/components/BlogArticle.jsx
+++ client/src/components/BlogArticle.jsx
@@ -2,30 +2,35 @@ import React from "react";
 import "./blogArticle.scss";
 import "./lessonCard.scss";
 import { Card, Row, Col } from "reactstrap";
-import Accountant from "../assets/accountant.png";
+import Yea from "../assets/yea.png";
 
 const BlogArticle = () => {
+  const currentDate = new Date();
   return (
     <Card className="article-card">
       <Row className="g-0">
         <Col md="5" className="image-col">
-          <img src={Accountant} alt="card" fluid className="article-img" />
+          <img src={Yea} alt="card" fluid className="article-img" />
         </Col>
 
         <Col md="7">
           <div md="2" className="article-card-body">
-            <h1 className="article-title">Card title</h1>
+            <h1 className="article-title">Congratulations, you made it!</h1>
             <p className="name-date">
-              Tina Turner <span>01.04.23</span>
+              Chat GPT
+              <span>
+                {" "}
+                {` ${currentDate.getDate()}/${
+                  currentDate.getMonth() + 1
+                }/${currentDate.getFullYear()}`}
+              </span>
             </p>
 
             <p md="6" className="article-text">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
-            <p md="2" className="article-footer">
-              Last updated 3 mins ago
+              Congratulations to the recent graduates of the Full Stack Web
+              Development course! Your hard work and dedication have paid off,
+              and you should be proud of your achievements in gaining valuable
+              skills and experience in web development.
             </p>
           </div>
         </Col>
--- client/src/pages/Post.jsx
+++ client/src/pages/Post.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import Accountant from "../assets/accountant.png";
+import Yea from "../assets/yea.png";
 
 // const { id } = useParams();
 
@@ -22,27 +22,51 @@ const Post = () => {
       <div className="post-section">
         <div className="image">
           {/* <Link to={`/post/${_id}`}> */}
-          <img src={Accountant} alt="card" className="post-img" />
+          <img src={Yea} alt="card" className="post-img1" />
           {/* </Link> */}
         </div>
         <div className="texts">
           {/* <Link to={`/post/${_id}`}> */}
-          <h1>Title</h1>
+          <h1 className="post-title">Congratulations, you made it!</h1>
           {/* </Link> */}
-          <p className="info">
-            <p className="author"> Author username</p>
+          <p className="info" md={6}>
+            <p className="author"> Chat GPT</p>
             <p>
-              {`${currentDate.getDate()}/${
+              {` ${currentDate.getDate()}/${
                 currentDate.getMonth() + 1
-              }/${currentDate.getFullYear()} @ ${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`}
+              }/${currentDate.getFullYear()}`}
             </p>
           </p>
           <p className="content">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Consequuntur similique ut debitis error, beatae consequatur officiis
-            voluptas officia maiores iusto ipsam recusandae neque qui! Rem a,
-            perferendis quis cum esse, beatae consequuntur reprehenderit labore
-            atque tenetur fuga facere impedit mollitia.{" "}
+            To all the students who recently completed the Full Stack Web
+            Development course at DCI Digital Career Institute Berlin,
+            congratulations! This is a tremendous achievement, and you should be
+            proud of yourselves for all the hard work and dedication you put
+            into this course. Over the past year, you have gained a wealth of
+            knowledge and practical experience in web development. You have
+            learned about HTML, CSS, JavaScript, React, Node.js, databases, and
+            more. You have worked on real-world projects and developed the
+            skills necessary to succeed in the industry.
+            <br />
+            <br />
+            But your accomplishments go beyond just the technical skills you
+            acquired. You have also demonstrated a strong work ethic,
+            perseverance, and a willingness to learn and grow. These are
+            qualities that will serve you well throughout your career. I hope
+            that you take the time to reflect on your achievements and celebrate
+            your success. Completing a Full Stack Web Development course is no
+            small feat, and you should be proud of yourselves for making it to
+            the finish line.
+            <br />
+            <br />
+            As you move forward in your career, remember that learning is a
+            lifelong journey. The skills you acquired during this course will
+            serve as a foundation for your future growth and development. Keep
+            pushing yourself, stay curious, and never stop learning. Finally, I
+            want to extend my congratulations once again. You have earned this
+            achievement, and I have no doubt that you will go on to achieve even
+            greater things in your careers. Good luck, and I wish you all the
+            best in your future endeavors!
           </p>
         </div>
       </div>
